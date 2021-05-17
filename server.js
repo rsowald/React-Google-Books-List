f3ca62490e12ecb534d84fc72db23f36ff4c6497
rsowald <72467920+rsowald@users.noreply.github.com>
2021-05-17 11:01:45 -0700

    set up routes

--- server.js
+++ server.js
@@ -2,22 +2,22 @@ const express = require("express");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
+const routes = require("./routes");
 const mongoose = require("mongoose");
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
+// Add routes, both API and view
+app.use(routes);
+
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks", {
   useNewUrlParser: true,
   useFindAndModify: false
-});
-
-// Send every request to the React app
-// Define any API routes before this runs
-app.get("*", function (req, res) {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
 app.listen(PORT, function () {
