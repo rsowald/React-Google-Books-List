733a26aab487bf8ff988c7c947882dae96c5bb0a
rsowald <72467920+rsowald@users.noreply.github.com>
2021-05-17 11:18:53 -0700

    wrote books controller methods to match routes

--- models/Book.js
+++ models/Book.js
@@ -6,7 +6,8 @@ const bookSchema = new Schema({
     author: { type: String, required: true },
     description: { type: String, required: true },
     image: { type: String, required: false },
-    link: { type: String, required: true }
+    link: { type: String, required: true },
+    date: { type: Date, default: Date.now }
 });
 
 const Book = mongoose.model("Book", bookSchema);
