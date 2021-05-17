20ad4dc0276f862c5166d77c123a0b42f25676d9
rsowald <72467920+rsowald@users.noreply.github.com>
2021-05-17 14:42:01 -0700

    made bootstrap navbar

---
+++ client/src/components/Navbar.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import { Link } from 'react-router-dom';
+
+function MyNav() {
+    return (
+        <Navbar collapseOnSelect expand="md" bg="primary" variant="light">
+            <Navbar.Brand as={Link} to="/" >My Reading List</Navbar.Brand>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse id="responsive-navbar-nav">
+                <Nav className="mr-auto">
+                    <Nav.Link as={Link} to="/search" >Search</Nav.Link>
+                    <Nav.Link as={Link} to="/saved" >Saved</Nav.Link>
+                </Nav>
+            </Navbar.Collapse>
+        </Navbar>
+    )
+}
+
+export default MyNav;
