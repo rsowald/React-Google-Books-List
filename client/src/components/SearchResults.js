315d8797f0b884c5c4819969c4cba6f892eb0849
rsowald <72467920+rsowald@users.noreply.github.com>
2021-05-17 22:38:10 -0700

    made search result card

---
+++ client/src/components/SearchResults.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Card, Button } from 'react-bootstrap';
+
+function SearchResults(props) {
+    return (
+        <Card>
+            <Card.Header>
+                <h3>{props.title}</h3>
+                <p>by {props.authors}</p>
+                <Button variant="primary" onClick={props.handleView}>View</Button>
+                <Button variant="primary" onClick={props.handleSave}>Save</Button>
+            </Card.Header>
+            <Card.Body>
+                <img src={props.thumbnail} alt={props.title} />
+                <p> {props.description} </p>
+            </Card.Body>
+        </Card>
+    );
+}
+
+export default SearchResults;
