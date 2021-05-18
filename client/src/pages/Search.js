9dfeb36bd7a550c0f99790d09d990faf1df4a7e7
rsowald <72467920+rsowald@users.noreply.github.com>
2021-05-18 11:32:41 -0700

    added more handle functions and conditionally render results container

--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -1,13 +1,35 @@
 import React, { useState } from 'react';
 import { Container, Row, Col, Form, Button } from 'react-bootstrap';
 import SearchResults from "../components/SearchResults";
+import API from '../utils/API';
 
 function Search() {
 
-    const [search, setSearch] = useState("");
-    const [results, setResults] = useState([])
+    const [query, setQuery] = useState("");
+    const [results, setResults] = useState([]);
+    const [showResults, setShowResults] = useState(false);
 
-    function handleSearch() {
+    function handleInputChange(event) {
+        // Destructure the name and value properties off of event.target
+        // Update the query state
+        const { value } = event.target;
+        setQuery(value);
+    }
+
+    function handleSearch(event) {
+        event.preventDefault();
+        console.log(query);
+        API.search(query)
+            .then(res => {
+                console.log(res.data);
+                setResults(res.data.items);
+                console.log(results.length);
+            })
+            .catch(err => console.log(err));
+        setShowResults(true)
+    }
+
+    function handleSave() {
 
     }
 
@@ -18,38 +40,43 @@ function Search() {
                     <Form style={{ padding: "10px" }}>
                         <Form.Group controlId="searchForm.Input">
                             <Form.Label>Search Terms</Form.Label>
-                            <Form.Control type="input" />
+                            <Form.Control type="text" onChange={handleInputChange} placeholder="Keyword? Title? Author? Whatever." />
                         </Form.Group>
-                        <Form.Group controlId="searchForm.Select">
+                        {/* <Form.Group controlId="searchForm.Select">
                             <Form.Label>Search By</Form.Label>
                             <Form.Control as="select">
                                 <option value="title">Title</option>
                                 <option value="author">Author</option>
                                 <option value="genre">Genre</option>
                             </Form.Control>
-                        </Form.Group>
+                        </Form.Group> */}
+                        <Button variant="primary" className="float-right mb-2" onClick={handleSearch}>Search</Button>
                     </Form>
                 </Col>
             </Row>
-            <Row style={{ border: "3px solid black", borderRadius: "5px", marginTop: "20px" }}>
-                <Col size="md-8">
-                    {results.length ?
-                        (results.map(book => (
-                            <SearchResults
-                                style={{ padding: "10px" }}
-                                title={book.title}
-                                authors={book.authors}
-                                selfLink={book.selfLink}
-                                thumbnail={book.imageLinks.thumbnail}
-                                description={book.description}
-                            ></SearchResults>))
-                        ) : (
-                            <h3>No matching results found. Please try different search criteria.</h3>
-                        )}
-                </Col>
 
-            </Row>
-            {/* <Button variant="primary" onClick={handleSearch}>Search</Button> */}
+            {showResults ? (
+                <Row style={{ border: "3px solid black", borderRadius: "5px", marginTop: "20px" }}>
+                    <Col size="md-8">
+                        {results.length ?
+                            (results.map(book => (
+                                <SearchResults
+                                    style={{ padding: "10px" }}
+                                    title={book.items.volumeInfo.title}
+                                    authors={book.items.volumeInfo.authors}
+                                    selfLink={book.items.selfLink}
+                                    thumbnail={book.items.volumeInfo.imageLinks.thumbnail}
+                                    description={book.items.volumeInfo.description}
+                                    handleSave={handleSave}
+                                ></SearchResults>))
+                            ) : (
+                                <h3>No matching results found. Please try different search criteria.</h3>
+                            )}
+                    </Col>
+                </Row>
+            ) : (<></>)}
+
+
         </Container >
     )
 }
