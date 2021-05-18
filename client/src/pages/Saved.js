179f3efbc155206547fd474c221ee9838485d927
rsowald <72467920+rsowald@users.noreply.github.com>
2021-05-18 00:01:47 -0700

    fixing thumbnail source and no books found message

--- client/src/pages/Saved.js
+++ client/src/pages/Saved.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import {Container, Row, Col} from 'react-bootstrap';
+import { Container, Row, Col } from 'react-bootstrap';
 import SavedResults from "../components/SavedResults";
 import API from "../utils/API";
 
@@ -11,16 +11,16 @@ function Saved() {
         loadBooks();
     }, []);
 
-    function loadBooks(){
+    function loadBooks() {
         API.getBooks()
-            .then(res=> {
-            console.log(res.data)
-            setBooks(res.data)
+            .then(res => {
+                console.log(res.data)
+                setBooks(res.data)
             })
-            .catch(err=>console.error(err));
+            .catch(err => console.error(err));
     };
 
-    function deleteBook(id){
+    function deleteBook(id) {
         API.deleteBook(id)
             .then(res => loadBooks())
             .catch(err => console.error(err));
@@ -28,7 +28,7 @@ function Saved() {
 
     return (
         <Container>
-            <Row style={{marginTop: "20px"}}>
+            <Row style={{ marginTop: "20px" }}>
                 <Col size="md-8">
                     {books.length ?
                         (books.map(book => (
@@ -36,15 +36,17 @@ function Saved() {
                                 title={book.title}
                                 authors={book.authors}
                                 selfLink={book.selfLink}
-                                thumbnail={book.thumbnail}
+                                thumbnail={book.imageLinks.thumbnail}
                                 description={book.description}
                                 handleDelete={deleteBook(book._id)}
                             >
                             </SavedResults>))
-                            ) : (
-                                <h3 align="center">You haven't saved any books yet.<h3>
-                                    </h3>Go search for some!</h3>
-                            )}
+                        ) : (
+                            <>
+                                <h3 align="center">You haven't saved any books yet.</h3>
+                                <h3 align="center">Go search for some!</h3>
+                            </>
+                        )}
                 </Col>
             </Row>
         </Container>
