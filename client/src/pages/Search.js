0ad00c7364c85aabb7b7c43b274e51aa8db4e597
rsowald <72467920+rsowald@users.noreply.github.com>
2021-05-18 00:07:51 -0700

    conditionally render results cards

--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -1,8 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Container, Row, Col, Form, Button } from 'react-bootstrap';
 import SearchResults from "../components/SearchResults";
 
 function Search() {
+
+    const [search, setSearch] = useState("");
+    const [results, setResults] = useState([])
+
+    function handleSearch() {
+
+    }
+
     return (
         <Container>
             <Row style={{ border: "3px solid black", borderRadius: "5px", marginTop: "20px" }}>
@@ -15,16 +23,31 @@ function Search() {
                         <Form.Group controlId="searchForm.Select">
                             <Form.Label>Search By</Form.Label>
                             <Form.Control as="select">
-                                <option>Title</option>
-                                <option>Author</option>
-                                <option>Genre</option>
+                                <option value="title">Title</option>
+                                <option value="author">Author</option>
+                                <option value="genre">Genre</option>
                             </Form.Control>
                         </Form.Group>
                     </Form>
                 </Col>
             </Row>
             <Row style={{ border: "3px solid black", borderRadius: "5px", marginTop: "20px" }}>
-                <SearchResults style={{ padding: "10px" }} />
+                <Col size="md-8">
+                    {results.length ?
+                        (results.map(book => (
+                            <SearchResults
+                                style={{ padding: "10px" }}
+                                title={book.title}
+                                authors={book.authors}
+                                selfLink={book.selfLink}
+                                thumbnail={book.imageLinks.thumbnail}
+                                description={book.description}
+                            ></SearchResults>))
+                        ) : (
+                            <h3>No matching results found. Please try different search criteria.</h3>
+                        )}
+                </Col>
+
             </Row>
             {/* <Button variant="primary" onClick={handleSearch}>Search</Button> */}
         </Container >
