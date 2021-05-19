41cd47fd51911418060a2ec54db24f014f889a8d
rsowald <72467920+rsowald@users.noreply.github.com>
2021-05-19 10:52:03 -0700

    fixing db model connection

--- controllers/booksController.js
+++ controllers/booksController.js
@@ -1,22 +1,23 @@
-const db = require("../models/Book.js");
+const Book = require("../models/Book.js");
 
 // Defining methods for the booksController
 module.exports = {
     findAll: function (req, res) {
-        db.Book
+        Book
             .find()
             .sort({ date: -1 })
             .then(dbModel => res.json(dbModel))
             .catch(err => res.status(422).json(err));
     },
     create: function (req, res) {
-        db.Book
+        console.log(req.body)
+        Book
             .create(req.body)
             .then(dbModel => res.json(dbModel))
-            .catch(err => res.status(422).json(err));
+            .catch(err => console.log(err));
     },
     remove: function (req, res) {
-        db.Book
+        Book
             .findById({ _id: req.params.id })
             .then(dbModel => dbModel.remove())
             .then(dbModel => res.json(dbModel))
